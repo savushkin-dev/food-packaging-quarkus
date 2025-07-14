32169387403f2cdb0654209e99dff43d9f415849
redCat <yana.sitkowecz@yandex.ru>
2025-07-14 09:05:49 +0300

    Rename DemoData24Generator by LoadData

--- src/main/java/org/acme/foodpackaging/bootstrap/LoadData.java
+++ src/main/java/org/acme/foodpackaging/bootstrap/LoadData.java
@@ -14,7 +14,7 @@ import java.time.*;
 import java.util.*;
 
 @ApplicationScoped
-public class DemoData24Hours {
+public class LoadData {
     @Inject
     PackagingScheduleRepository repository;
 
@@ -293,4 +293,3 @@ public class DemoData24Hours {
         );
     }
 }
-
