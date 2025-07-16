25099e0c8c991e9eac97f280bf982a09e0a2ade5
redCat <yana.sitkowecz@yandex.ru>
2025-07-16 09:10:03 +0300

    Fix sql query format

--- src/main/java/org/acme/foodpackaging/sql/SqlQueries.java
+++ src/main/java/org/acme/foodpackaging/sql/SqlQueries.java
@@ -7,11 +7,10 @@ public class SqlQueries {
     }
 
     public static final String LOAD_JOBS = """
-            "SELECT v.KSK, v.SNPZ, v.DTI, v.DTM, v.KMC, v.EMK, v.KOLMV, v.MASSA, v.KOLEV, v.NP, v.UX, "
-                  + "m.MASSA, m.EAN13, m.SNM, m.NAME "
-                  + "FROM [MES].[dbo].[BD_VZPMC] as v, NS_MC as m "
-                  + "WHERE (v.KMC = m.KMC) AND (v.DTI = ?) AND (v.KSK = ?) AND (m.MASSA < ?) "
-                  + "ORDER BY v.SNPZ";
-        """;
-
+    SELECT v.KSK, v.SNPZ, v.DTI, v.DTM, v.KMC, v.EMK, v.KOLMV, v.MASSA, v.KOLEV, v.NP, v.UX,
+           m.MASSA, m.EAN13, m.SNM, m.NAME
+    FROM [MES].[dbo].[BD_VZPMC] AS v, NS_MC AS m
+    WHERE (v.KMC = m.KMC) AND (v.DTI = ?) AND (v.KSK = ?) AND (m.MASSA < ?)
+    ORDER BY v.SNPZ
+    """;
 }
