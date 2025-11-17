93193ad6f58473e223119bed62778479756ccf2f
redCat <yana.sitkowecz@yandex.ru>
2025-11-17 10:05:42 +0300

    Added methos for new job

--- src/main/java/org/acme/foodpackaging/bootstrap/LoadData.java
+++ src/main/java/org/acme/foodpackaging/bootstrap/LoadData.java
@@ -60,6 +60,17 @@ public class LoadData {
         Set<Product> productSet = new HashSet<>();
         List<Job> jobs = loadJobs(String.valueOf(START_DATE), MIN_START_DATE_TIME, productSet);
         List<Product> products = new ArrayList<>(productSet);
+        Product maintenanceProduct = new Product(
+                "Maintenance Product",
+                 "MAINTENANCE",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""
+        );
+        products.add(maintenanceProduct);
         // Инициализация времени мойки между продукцией
         calculator.cleaningCalculate(products);
         solution.setLines(lines);
