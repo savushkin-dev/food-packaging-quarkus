dc728d7e12494a54a101c7f9c021d7981fa56c41
redCat <yana.sitkowecz@yandex.ru>
2025-11-19 14:24:20 +0300

    Fix cleaning time before maintenance

--- src/main/java/org/acme/foodpackaging/domain/CleaningCalculator.java
+++ src/main/java/org/acme/foodpackaging/domain/CleaningCalculator.java
@@ -73,7 +73,7 @@ public class CleaningCalculator {
             Map<Product, Duration> durations = new HashMap<>(products.size());
             for (Product previous : products) {
                 Duration duration;
-                if(current.getId().equals("MAINTENANCE")){
+                if(current.getId().equals("MAINTENANCE") || previous.getId().equals("MAINTENANCE")){
                     duration = Duration.ZERO;
                 }
               else if(current.getId().equals(previous.getId())){
