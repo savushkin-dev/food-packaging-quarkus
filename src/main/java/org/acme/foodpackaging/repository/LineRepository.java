3181c422a56de8f14a4ba60ee38106db1d56749a
redCat <yana.sitkowecz@yandex.ru>
2025-12-09 16:59:56 +0300

    Made load lines with panache

--- src/main/java/org/acme/foodpackaging/repository/LineRepository.java
+++ src/main/java/org/acme/foodpackaging/repository/LineRepository.java
@@ -1,36 +1,25 @@
 package org.acme.foodpackaging.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.acme.foodpackaging.sql.SqlQueries.LOAD_LINES_WITH_NAME;
+import org.acme.foodpackaging.entity.LineEntity;
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
-public class LineRepository {
+public class LineRepository  implements PanacheRepository<LineEntity> {
+    /**
+     * Загружает id и название линии
+     */
+    public ConcurrentMap<String, String> loadLines() {
+        List<LineEntity> lines = list("fDel = 0");
 
-    @Inject
-    @ConfigProperty(name = "db.url")
-    String dbUrl;
-
-    public Map<String, String> loadLines() {
-        Map<String, String> lines = new HashMap<>();
-
-        try (Connection connection = DriverManager.getConnection(dbUrl);
-             Statement statement = connection.createStatement();
-             ResultSet rs = statement.executeQuery(LOAD_LINES_WITH_NAME)) {
-
-            while (rs.next()) {
-                lines.put(rs.getString("KRC"), rs.getString("SNM"));
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to load lines from DB", e);
-        }
-        return lines;
+        return lines.stream()
+                .collect(Collectors.toConcurrentMap(
+                        e -> e.getKrc().trim(),
+                        e -> e.getSnm().trim(),
+                        (existing, replacement) -> existing
+                ));
     }
 }
