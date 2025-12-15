3fbf08fdf69bb6876e02a89efa99cccd833d8947
NikolayPunko <punkon2001@gmail.com>
2025-12-15 16:41:49 +0300

    New implementation of plan preservation

---
+++ src/main/java/org/acme/foodpackaging/entity/jobs/OeePevEntity.java
@@ -0,0 +1,51 @@
+package org.acme.foodpackaging.entity.jobs;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "OEE_PEV", schema = "dbo")
+public class OeePevEntity extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "F_ID")
+    private long fId;
+
+    @Column(name = "KRC", columnDefinition = "CHAR(12)")
+    private String krc;
+
+    @Column(name = "PDTN")
+    private LocalDateTime pdtn;
+
+    @Column(name = "PDTO")
+    private LocalDateTime pdto;
+
+    @Column(name = "PDUR")
+    private Integer pdur;
+
+    @Column(name = "SNPZ")
+    private Integer snpz;
+
+    @Column(name = "EVTYPE")
+    private Integer evtype;
+
+    @Column(name = "REASON")
+    private Integer reason;
+
+    @Column(name = "NOTE")
+    private String note;
+
+
+}
