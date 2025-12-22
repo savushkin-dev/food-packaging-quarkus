aab1a9a2b8a7f19b0c508498f2386666626bf9c7
NikolayPunko <punkon2001@gmail.com>
2025-12-22 10:42:11 +0300

    Added and configured parameters ksk, krc, krca via the configuration file

--- src/main/java/org/acme/foodpackaging/repository/products/CleaningRuleRepository.java
+++ src/main/java/org/acme/foodpackaging/repository/products/CleaningRuleRepository.java
@@ -2,19 +2,25 @@ package org.acme.foodpackaging.repository.products;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.acme.foodpackaging.domain.CleaningRule;
 import org.acme.foodpackaging.entity.products.CleaningRuleEntity;
+import org.acme.foodpackaging.sql.SqlQueries;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 
 @ApplicationScoped
 public class CleaningRuleRepository implements PanacheRepository<CleaningRuleEntity> {
 
+    @ConfigProperty(name = "krc")
+    String krc;
+
     public List<CleaningRule> loadRules() {
 
         List<CleaningRuleEntity> rows =
                 find("deletedFlag = 0 and krc = ?1 and duration is not null order by npar",
-                        "170610000000")
+                        krc)
                         .list();
 
         return rows.stream()
