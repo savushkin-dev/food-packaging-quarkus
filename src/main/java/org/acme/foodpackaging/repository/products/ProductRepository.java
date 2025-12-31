0809d777ae1a6b1c0e5f1f8d77147e2688183e16
redCat <yana.sitkowecz@yandex.ru>
2025-12-31 12:43:01 +0300

    fix query in product loading

--- src/main/java/org/acme/foodpackaging/repository/products/ProductRepository.java
+++ src/main/java/org/acme/foodpackaging/repository/products/ProductRepository.java
@@ -40,7 +40,7 @@ public Map<String, Product> loadProducts() {
             p.mass, p.filling, p.ns.shortName,
             p.ns.krkmc
         )
-        from ProductEntity p
+        from PlrMc p
         join p.ns n
         where p.deletedFlag = 0
     """, ProductRow.class).getResultList();
