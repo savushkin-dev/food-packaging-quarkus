d2524c2a88253ec5ebd7247b4dc9a1c56c6e08cd
redCat <yana.sitkowecz@yandex.ru>
2026-01-23 08:30:08 +0300

    Remove customException

--- src/main/java/org/acme/foodpackaging/exception/UploadDataException.java
+++
@@ -1,7 +0,0 @@
-package org.acme.foodpackaging.exception;
-
-public class UploadDataException extends RuntimeException {
-    public UploadDataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
