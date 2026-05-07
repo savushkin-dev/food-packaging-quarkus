b2b5ddaf0a636b6fde49761f9557fc539d344771
Магамадов Муслим Раджабович <Magamadov@bmk.by>
2026-05-07 17:02:11 +0300

    updating feature: Added API method for reading batch downtime list

--- src/main/java/org/acme/foodpackaging/record/PmLogMarkingRow.java
+++
@@ -1,8 +0,0 @@
-package org.acme.foodpackaging.record;
-
-import java.time.LocalDateTime;
-
-/**
- * record для строк из PM_LOG(F_ID, DTS)
- */
-public record PmLogMarkingRow(long fId, LocalDateTime dts) {}
