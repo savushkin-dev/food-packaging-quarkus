1d58990dc64318647de4e690f7192632bd958068
redCat <yana.sitkowecz@yandex.ru>
2026-06-16 16:56:12 +0300

    Removed setLineStartDateTime from ScheduleUtils

--- src/main/java/org/acme/foodpackaging/scheduleoperations/utils/ScheduleUtils.java
+++ src/main/java/org/acme/foodpackaging/scheduleoperations/utils/ScheduleUtils.java
@@ -77,20 +77,7 @@ public class ScheduleUtils {
                 .findFirst()
                 .orElse(null);
     }
-    /**
-     * Меняет время старта линии
-     */
-    public static void setLineStartDateTime(Line line, LocalDateTime lineStartDateTime) {
-        line.setStartDateTime(lineStartDateTime);
-        if (line.getJobs() != null
-                && !line.getJobs().isEmpty()
-                && line.getJobs().getLast() != null
-                && line.getJobs().getLast().getEndDateTime() != null) {
 
-            line.setMaxEndTime(
-                    line.getJobs().getLast().getEndDateTime().plusHours(24));
-        }
-    }
     /**
      * Меняет максимальное время завершения работы линии
      */
