80d1cd4fcba07fb5e4eebb883c65033264af610f
Yana Sitkovets <yana.sitkowecz@yandex.ru>
2026-09-07 16:06:13 +0300

    Refactor Joblist init (#308)
    * docker : add cache for dependency
    
    * docker : default profile
    
    * refactor : Job init
    
    * test : codecoverage for MoveJobService, JobService, JobNoteService
    
    * fix : sonar issues
    
    * refactor : Job constructor for production Job
    
    * refactor : moved JobRow in dto/row/jobs package

--- src/test/java/builder/JobTestBuilder.java
+++ src/test/java/builder/JobTestBuilder.java
@@ -22,8 +22,13 @@ public class JobTestBuilder {
         return this;
     }
 
-     public JobTestBuilder withIdBatch(String idBatch) {
-        job.setIdBatch(idBatch);
+    public JobTestBuilder withLineId(String id) {
+        job.setLineId(id);
+        return this;
+    }
+
+    public JobTestBuilder withIdBatch(String id) {
+        job.setIdBatch(id);
         return this;
     }
 
@@ -58,6 +63,16 @@ public class JobTestBuilder {
 
     public JobTestBuilder withCamera(LocalDateTime start, LocalDateTime end) {
         job.setCameraStart(start);
+        job.setCameraEnd(end);
+        return this;
+    }
+
+    public JobTestBuilder withCameraStart(LocalDateTime start) {
+        job.setCameraStart(start);
+        return this;
+    }
+
+    public JobTestBuilder withCameraEnd(LocalDateTime end) {
         job.setCameraEnd(end);
         return this;
     }
